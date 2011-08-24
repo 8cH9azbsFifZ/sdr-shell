f5169b224df88e22a1995e5b1dc6f3fdbc5baab3
Dr. Gerolf Ziegenhain <g@ziegenhain.com>
2011-08-24 22:35:41 +0200

    initial commit

---
+++ sdr-shell-v2/memorycell.h
@@ -0,0 +1,44 @@
+#ifndef MEMORYCELL_H
+#define MEMORYCELL_H
+
+#include <qlabel.h>
+#include <qstring.h>
+
+class MemoryCell : public QLabel
+{
+    Q_OBJECT
+    
+    public:
+        MemoryCell(QWidget *parent = 0, const char *name = 0);
+
+        void setID( int );
+        void setMemo( QString );
+        void setFrequency( long long int );
+        void setMode( int );
+        void setFilter( int, int );
+        
+        int getID();
+        int getMemo();
+        long long int getFrequency();        
+        int getMode();
+        int getFilter_l();
+        int getFilter_h(); 
+        
+    private:
+        int id;
+        QString memo;
+        long long int frequency;
+        int mode;
+        int filter_l;
+        int filter_h;
+
+    protected:
+        void mouseReleaseEvent ( QMouseEvent * );
+        void enterEvent ( QEvent * );
+
+    signals:
+        void read( MemoryCell * );
+        void write( MemoryCell * );
+        void display( MemoryCell * );
+};
+#endif
