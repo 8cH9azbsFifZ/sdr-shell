885930a3a6b1aa1b74c44490f723ff532c963ca3
Dr. Gerolf Ziegenhain <g@ziegenhain.com>
2011-08-27 16:54:38 +0200

    documentation

--- sdr-shell-v4/src/startup/start_softrock_lite.sh
+++ sdr-shell-v4/src/startup/start_softrock_lite.sh
@@ -1,12 +1,26 @@
 #!/bin/sh
-clear
 # Start-Up for sdr-shell, dttsp, and usbsoftrock.  Oriented towards a Softrock
 # RXTX board.
+clear
 # http://sdr-shell.googlecode.com/svn/tags/PMSDR-1.0/sdr-shell.sh
 set -x
 export SDR_DEFRATE=96000 #48000 
 DEV_CAPTURE=hw:1
 DEV_PLAYBACK=hw:0
+export SDR_NAME=sdr
+export SDR_NAME=sdr-tx # double entry?
+# communication over ports
+export SDR_PARMPORT=19005 # sdr-core eats this
+export SDR_SPECPORT=19006  # sdr-core eats this
+export SDR_METERPORT=19007  # sdr-core eats this
+
+## FIFOs
+# if communication over fifos
+#export SDR_PARMPATH=/dev/shm/SDRcommands
+#export SDR_METERPATH=/dev/shm/SDRmeter
+#export SDR_SPECPATH=/dev/shm/SDRspectrum
+# mkfifo /dev/shm/SDRcommands
+
 
 ## Start jackd
 /usr/bin/jackd -R -P66 -p128 -t2000 -dalsa -r$SDR_DEFRATE -D -C$DEV_CAPTURE -P$DEV_PLAYBACK -s -p 2048&
@@ -30,7 +44,6 @@ sleep 2
 #modprobe realcap any=1 allcaps=1
 
 ## Start DttSP
-export SDR_NAME=sdr
 sdr-core --sprectrum --metering&
 #/usr/local/bin/sdr-core --spectrum --metering --client-name=${NAME}_RX --buffsize=${JACK_BUFFER} --ringmult=4 --command-port=19001 --spectrum-port=19002 --meter-port=19003
 PIDS="$PIDS $!"
@@ -43,17 +56,6 @@ jack_connect sdr:or alsa_pcm:playback_2
 jack_connect alsa_pcm:capture_1 sdr:il        
 jack_connect alsa_pcm:capture_2 sdr:ir     
 jack_lsp -c
-
-export SDR_NAME=sdr-tx # double entry?
-export SDR_PARMPORT=19005 # sdr-core eats this
-export SDR_SPECPORT=19006  # sdr-core eats this
-export SDR_METERPORT=19007  # sdr-core eats this
-
-# FIFOs
-export SDR_PARMPATH=/dev/shm/SDRcommands
-export SDR_METERPATH=/dev/shm/SDRmeter
-export SDR_SPECPATH=/dev/shm/SDRspectrum
-# mkfifo /dev/shm/SDRcommands
 
 
 sdr-core -s -m -v &
