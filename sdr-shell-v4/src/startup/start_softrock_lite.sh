8a4508d877d764ca5652c8b55555b7a40ec1e686
Dr. Gerolf Ziegenhain <g@ziegenhain.com>
2011-08-27 16:46:10 +0200

    documentation

--- sdr-shell-v4/src/startup/start_softrock_lite.sh
+++ sdr-shell-v4/src/startup/start_softrock_lite.sh
@@ -3,12 +3,12 @@ clear
 # Start-Up for sdr-shell, dttsp, and usbsoftrock.  Oriented towards a Softrock
 # RXTX board.
 # http://sdr-shell.googlecode.com/svn/tags/PMSDR-1.0/sdr-shell.sh
-
 set -x
 export SDR_DEFRATE=96000 #48000 
 DEV_CAPTURE=hw:1
 DEV_PLAYBACK=hw:0
 
+## Start jackd
 /usr/bin/jackd -R -P66 -p128 -t2000 -dalsa -r$SDR_DEFRATE -D -C$DEV_CAPTURE -P$DEV_PLAYBACK -s -p 2048&
 # R - realtime
 # P - realtime priority
@@ -21,54 +21,44 @@ DEV_PLAYBACK=hw:0
 # P - alsa option: playback if
 # s - alsa option: softmode
 # p - period: default 1024
-
-# Realtime
-#  rmmod capability
-#  rmmod commoncap
-#  modprobe realcap any=1 allcaps=1
-
-
 JPID="$!"
-
 sleep 2
 
+## Realtime
+#rmmod capability
+#rmmod commoncap
+#modprobe realcap any=1 allcaps=1
+
+## Start DttSP
 export SDR_NAME=sdr
-# prepare:
-# mkfifo /dev/shm/SDRcommands
-sdr-core -s -m&
+sdr-core --sprectrum --metering&
+#/usr/local/bin/sdr-core --spectrum --metering --client-name=${NAME}_RX --buffsize=${JACK_BUFFER} --ringmult=4 --command-port=19001 --spectrum-port=19002 --meter-port=19003
 PIDS="$PIDS $!"
 
 sleep 1
 
 # connect jack
-#echo "  sdr-$DTTSP_PID:ol -> alsa_pcm:playback_1"
-#$JACKC sdr-$DTTSP_PID:ol alsa_pcm:playback_1
-#echo "  sdr-$DTTSP_PID:or -> alsa_pcm:playback_2"
-#$JACKC sdr-$DTTSP_PID:or alsa_pcm:playback_2
-#echo "  alsa_pcm:capture_1 -> sdr-$DTTSP_PID:il"
-#$JACKC alsa_pcm:capture_1 sdr-$DTTSP_PID:il
-#echo "  alsa_pcm:capture_2 -> sdr-$DTTSP_PID:ir"
-#$JACKC alsa_pcm:capture_2 sdr-$DTTSP_PID:ir
-
 jack_connect sdr:ol alsa_pcm:playback_1 
 jack_connect sdr:or alsa_pcm:playback_2
 jack_connect alsa_pcm:capture_1 sdr:il        
 jack_connect alsa_pcm:capture_2 sdr:ir     
 jack_lsp -c
 
-export SDR_NAME=sdr-tx
-export SDR_PARMPORT=19005
-export SDR_SPECPORT=19006
-export SDR_METERPORT=19007
-# Path to DttSP command FIFO
+export SDR_NAME=sdr-tx # double entry?
+export SDR_PARMPORT=19005 # sdr-core eats this
+export SDR_SPECPORT=19006  # sdr-core eats this
+export SDR_METERPORT=19007  # sdr-core eats this
+
+# FIFOs
 export SDR_PARMPATH=/dev/shm/SDRcommands
-# Path to DttSP meter FIFO
 export SDR_METERPATH=/dev/shm/SDRmeter
-# Path to DttSP spectrum FIFO
 export SDR_SPECPATH=/dev/shm/SDRspectrum
+# mkfifo /dev/shm/SDRcommands
 
 
 sdr-core -s -m -v &
+#/usr/local/bin/sdr-core --spectrum --metering --client-name=${NAME}_RX --buffsize=${JACK_BUFFER} --ringmult=4 --command-port=19001 --spectrum-port=19002 --meter-port=19003&
+
 PIDS="$PIDS $!"
 sleep 1
 
@@ -93,7 +83,7 @@ esac
 
 echo $PIDS
 
-export SDR_MODE=$PWD/hook-mode
+#export SDR_MODE=$PWD/hook-mode
 #export SDR_BAND=$PWD/hook-band
 ./sdr-shell
 
