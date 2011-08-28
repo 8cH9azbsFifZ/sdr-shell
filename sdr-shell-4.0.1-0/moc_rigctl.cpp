cd1ccd002f8748c2da8b25385e1d2b64ec3b313a
Dr. Gerolf Ziegenhain <g@ziegenhain.com>
2011-08-28 02:23:29 +0200

    mv src to .

---
+++ sdr-shell-4.0.1-0/moc_rigctl.cpp
@@ -0,0 +1,140 @@
+/****************************************************************************
+** Meta object code from reading C++ file 'rigctl.h'
+**
+** Created: Sun Aug 28 02:17:47 2011
+**      by: The Qt Meta Object Compiler version 62 (Qt 4.6.3)
+**
+** WARNING! All changes made in this file will be lost!
+*****************************************************************************/
+
+#include "rigctl.h"
+#if !defined(Q_MOC_OUTPUT_REVISION)
+#error "The header file 'rigctl.h' doesn't include <QObject>."
+#elif Q_MOC_OUTPUT_REVISION != 62
+#error "This file was generated using the moc from 4.6.3. It"
+#error "cannot be used with the include files from this version of Qt."
+#error "(The moc has changed too much.)"
+#endif
+
+QT_BEGIN_MOC_NAMESPACE
+static const uint qt_meta_data_RigCtlSocket[] = {
+
+ // content:
+       4,       // revision
+       0,       // classname
+       0,    0, // classinfo
+       2,   14, // methods
+       0,    0, // properties
+       0,    0, // enums/sets
+       0,    0, // constructors
+       0,       // flags
+       0,       // signalCount
+
+ // slots: signature, parameters, type, tag, flags
+      14,   13,   13,   13, 0x0a,
+      29,   13,   13,   13, 0x0a,
+
+       0        // eod
+};
+
+static const char qt_meta_stringdata_RigCtlSocket[] = {
+    "RigCtlSocket\0\0disconnected()\0readyRead()\0"
+};
+
+const QMetaObject RigCtlSocket::staticMetaObject = {
+    { &QObject::staticMetaObject, qt_meta_stringdata_RigCtlSocket,
+      qt_meta_data_RigCtlSocket, 0 }
+};
+
+#ifdef Q_NO_DATA_RELOCATION
+const QMetaObject &RigCtlSocket::getStaticMetaObject() { return staticMetaObject; }
+#endif //Q_NO_DATA_RELOCATION
+
+const QMetaObject *RigCtlSocket::metaObject() const
+{
+    return QObject::d_ptr->metaObject ? QObject::d_ptr->metaObject : &staticMetaObject;
+}
+
+void *RigCtlSocket::qt_metacast(const char *_clname)
+{
+    if (!_clname) return 0;
+    if (!strcmp(_clname, qt_meta_stringdata_RigCtlSocket))
+        return static_cast<void*>(const_cast< RigCtlSocket*>(this));
+    return QObject::qt_metacast(_clname);
+}
+
+int RigCtlSocket::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
+{
+    _id = QObject::qt_metacall(_c, _id, _a);
+    if (_id < 0)
+        return _id;
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        switch (_id) {
+        case 0: disconnected(); break;
+        case 1: readyRead(); break;
+        default: ;
+        }
+        _id -= 2;
+    }
+    return _id;
+}
+static const uint qt_meta_data_RigCtlServer[] = {
+
+ // content:
+       4,       // revision
+       0,       // classname
+       0,    0, // classinfo
+       1,   14, // methods
+       0,    0, // properties
+       0,    0, // enums/sets
+       0,    0, // constructors
+       0,       // flags
+       0,       // signalCount
+
+ // slots: signature, parameters, type, tag, flags
+      14,   13,   13,   13, 0x0a,
+
+       0        // eod
+};
+
+static const char qt_meta_stringdata_RigCtlServer[] = {
+    "RigCtlServer\0\0newConnection()\0"
+};
+
+const QMetaObject RigCtlServer::staticMetaObject = {
+    { &QObject::staticMetaObject, qt_meta_stringdata_RigCtlServer,
+      qt_meta_data_RigCtlServer, 0 }
+};
+
+#ifdef Q_NO_DATA_RELOCATION
+const QMetaObject &RigCtlServer::getStaticMetaObject() { return staticMetaObject; }
+#endif //Q_NO_DATA_RELOCATION
+
+const QMetaObject *RigCtlServer::metaObject() const
+{
+    return QObject::d_ptr->metaObject ? QObject::d_ptr->metaObject : &staticMetaObject;
+}
+
+void *RigCtlServer::qt_metacast(const char *_clname)
+{
+    if (!_clname) return 0;
+    if (!strcmp(_clname, qt_meta_stringdata_RigCtlServer))
+        return static_cast<void*>(const_cast< RigCtlServer*>(this));
+    return QObject::qt_metacast(_clname);
+}
+
+int RigCtlServer::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
+{
+    _id = QObject::qt_metacall(_c, _id, _a);
+    if (_id < 0)
+        return _id;
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        switch (_id) {
+        case 0: newConnection(); break;
+        default: ;
+        }
+        _id -= 1;
+    }
+    return _id;
+}
+QT_END_MOC_NAMESPACE
