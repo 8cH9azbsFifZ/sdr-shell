5eaeed96bf11af5c9df6569cb9adbe8ead902094
Dr. Gerolf Ziegenhain <g@ziegenhain.com>
2011-08-31 01:04:45 +0200

    new build

--- sdr-shell-4.156/moc_main_widget.cpp
+++ sdr-shell-4.156/moc_main_widget.cpp
@@ -1,7 +1,7 @@
 /****************************************************************************
 ** Meta object code from reading C++ file 'main_widget.h'
 **
-** Created: Wed Aug 31 00:56:37 2011
+** Created: Wed Aug 31 01:04:26 2011
 **      by: The Qt Meta Object Compiler version 62 (Qt 4.6.3)
 **
 ** WARNING! All changes made in this file will be lost!
