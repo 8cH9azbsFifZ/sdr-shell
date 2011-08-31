177046a4d16c0bc71effda2aef1b21d274b5dc5a
Dr. Gerolf Ziegenhain <g@ziegenhain.com>
2011-08-31 23:41:02 +0200

    new build

--- sdr-shell_4.156/moc_command.cpp
+++ sdr-shell_4.156/moc_command.cpp
@@ -1,7 +1,7 @@
 /****************************************************************************
 ** Meta object code from reading C++ file 'command.h'
 **
-** Created: Wed Aug 31 01:24:28 2011
+** Created: Wed Aug 31 23:40:23 2011
 **      by: The Qt Meta Object Compiler version 62 (Qt 4.6.3)
 **
 ** WARNING! All changes made in this file will be lost!
